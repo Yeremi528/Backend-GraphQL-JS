cb635b9923ba270a1c26323254dcf2bd46595de9
Yeremi528 <geremiararaya@gmail.com>
2022-04-03 01:16:25 -0400

    awa

--- src/db/resolvers.js
+++ src/db/resolvers.js
@@ -1,16 +1,35 @@
 const { PrismaClient} = require("@prisma/client")
 const prisma = new PrismaClient()
+const bcryptjs = require("bcryptjs")
 
 const resolvers = {
     Mutation:{
         signup : async (_,{input}) => {
-            const {name,apellido,email,password} = input 
+            const {name,apellido,email,password} = input
+            //Validar que existe usuario
+            const userValidate = await prisma.user.findUnique({
+                where:{
+                    email
+                }
+            })
+            if(userValidate){
+                return{
+                    userErrors:[{
+                        message:"Usuario ya existente"
+                    }],user:null
+                }
+            }
+
+            //Hashear Clave
+            const hashedPassword =  await bcryptjs.hash(password,10)
+            
+            
             const user =  await prisma.user.create({
                 data:{
                     name,
                     apellido,
                     email,
-                    password
+                    password:hashedPassword
                 }
             })
             return {
